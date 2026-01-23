0f646259381abd96ac25470749af53158e9e58da
insub <j.j.kant1948@gmail.com>
2026-01-23 17:31:11 +0900

    add: request packet (timestamp, dummy data)

--- mandatory/sources/icmp.c
+++ mandatory/sources/icmp.c
@@ -6,42 +6,71 @@
 /*   By: insub <insub@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/19 19:01:20 by insub             #+#    #+#             */
-/*   Updated: 2026/01/23 17:16:54 by insub            ###   ########.fr       */
+/*   Updated: 2026/01/23 17:29:27 by insub            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/socket.h>
+#include <sys/time.h>
 #include <netdb.h>
+#include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
 #include <arpa/inet.h>
 
 #include "icmp.h"
 
+#define ICMP_HDR_SIZE	8
+#define ICMP_DATA_SIZE	56
+
+typedef struct s_icmp_packet
+{
+	struct icmphdr	hdr;
+	char			data[ICMP_DATA_SIZE];
+}	t_icmp_packet;
+
+static void	fill_icmp_data(char *data, int size)
+{
+	struct timeval	tv;
+	int				i;
+
+	gettimeofday(&tv, NULL);
+	memcpy(data, &tv, sizeof(tv));
+	i = sizeof(tv);
+	while (i < size)
+	{
+		data[i] = 0x10 + (i - sizeof(tv));
+		i++;
+	}
+}
+
 int		send_icmp_echo_request(int sockfd, const char *ip_addr)
 {
-    struct sockaddr_in dest_addr;
-    struct icmphdr icmp_hdr;
+	struct sockaddr_in	dest_addr;
+	t_icmp_packet		packet;
 
-    dest_addr.sin_family = AF_INET;
-    dest_addr.sin_port = 0;
-    inet_pton(AF_INET, ip_addr, &dest_addr.sin_addr);
-    
-    icmp_hdr.type = ICMP_ECHO;
-    icmp_hdr.code = 0;
-    icmp_hdr.checksum = 0;
-    icmp_hdr.un.echo.id = getpid() & 0xFFFF;
-    icmp_hdr.un.echo.sequence = 0;
-    icmp_hdr.checksum = calculate_checksum((unsigned short *)&icmp_hdr, sizeof(icmp_hdr));
+	dest_addr.sin_family = AF_INET;
+	dest_addr.sin_port = 0;
+	inet_pton(AF_INET, ip_addr, &dest_addr.sin_addr);
 
-    if (sendto(sockfd, &icmp_hdr, sizeof(icmp_hdr), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr)) < 0)
-    {
-        perror("send_icmp_echo_request - sendto");
-        return (-1);
-    }
+	memset(&packet, 0, sizeof(packet));
+	packet.hdr.type = ICMP_ECHO;
+	packet.hdr.code = 0;
+	packet.hdr.un.echo.id = getpid() & 0xFFFF;
+	packet.hdr.un.echo.sequence = 0;
+	fill_icmp_data(packet.data, ICMP_DATA_SIZE);
+	packet.hdr.checksum = calculate_checksum((unsigned short *)&packet,
+			sizeof(packet));
 
-    return (0);
+	if (sendto(sockfd, &packet, sizeof(packet), 0,
+			(struct sockaddr *)&dest_addr, sizeof(dest_addr)) < 0)
+	{
+		perror("send_icmp_echo_request - sendto");
+		return (-1);
+	}
+	return (0);
 }
 
 int		receive_icmp_echo_reply(int sockfd, char *buffer, int buf_size)

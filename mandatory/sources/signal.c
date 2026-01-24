e0542aa361aa284238d9f3604ef64204da86c365
insub <j.j.kant1948@gmail.com>
2026-01-24 17:21:43 +0900

    fix: minor bug

--- mandatory/sources/signal.c
+++ mandatory/sources/signal.c
@@ -6,17 +6,14 @@
 /*   By: insub <insub@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 17:09:18 by insub             #+#    #+#             */
-/*   Updated: 2026/01/24 17:18:09 by insub            ###   ########.fr       */
+/*   Updated: 2026/01/24 17:21:32 by insub            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
+#include <stdio.h>
 
-void	set_signal(void)
-{
-	signal(SIGINT, sig_handler);
-	signal(SIGQUIT, sig_handler);
-}
+#include "ping.h"
 
 void	sig_handler(int sig)
 {
@@ -30,4 +27,10 @@ void	sig_handler(int sig)
         printf("Received unknown signal on sig handler: %d\n", sig);
     }
     return ;
+}
+
+void	set_signal(void)
+{
+	signal(SIGINT, sig_handler);
+	signal(SIGQUIT, sig_handler);
 }
